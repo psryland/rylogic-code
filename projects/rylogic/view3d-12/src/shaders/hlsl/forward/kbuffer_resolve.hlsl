19cbb6463fb0d8c4c7a01360841a8b2f0232bf39
Paul Ryland <psryland@gmail.com>
2026-05-03 22:52:19 +1200

    Lerp blend OIA colour with base

--- projects/rylogic/view3d-12/src/shaders/hlsl/forward/kbuffer_resolve.hlsl
+++ projects/rylogic/view3d-12/src/shaders/hlsl/forward/kbuffer_resolve.hlsl
@@ -12,21 +12,21 @@ Texture2D<float4>  g_opaque_colour :register(t0);
 Texture2D<uint4>   g_alpha_colour :register(t1);
 Texture2D<uint4>   g_alpha_depth :register(t2);
 
-struct PSIn
+struct PSIn_KBufferResolve
 {
 	float4 ss_vert :SV_Position;
 	float2 tex0 :TEXCOORD0;
 };
 
-PSIn VSFullScreenTriangle(uint vid :SV_VertexID)
+PSIn_KBufferResolve VSFullScreenTriangle(uint vid :SV_VertexID)
 {
-	PSIn Out = (PSIn)0;
+	PSIn_KBufferResolve Out = (PSIn_KBufferResolve)0;
 	Out.tex0 = float2((vid << 1) & 2, vid & 2);
 	Out.ss_vert = float4(Out.tex0 * float2(2, -2) + float2(-1, 1), 0, 1);
 	return Out;
 }
 
-float PSDepthResolve(PSIn In) :SV_Depth
+float PSDepthResolve(PSIn_KBufferResolve In) :SV_Depth
 {
 	int2 pix = int2(In.ss_vert.xy);
 	float depth = 1.0f;
@@ -36,16 +36,18 @@ float PSDepthResolve(PSIn In) :SV_Depth
 	return depth;
 }
 
-float4 PSAlphaResolve(PSIn In) :SV_Target
+float4 PSAlphaResolve(PSIn_KBufferResolve In) :SV_Target
 {
 	int2 pix = int2(In.ss_vert.xy);
 	float4 base = g_opaque_colour.Load(int3(pix, 0));
 	uint4 alpha_colour = g_alpha_colour.Load(int3(pix, 0));
 	uint4 alpha_depth = g_alpha_depth.Load(int3(pix, 0));
 
+	// Merge the OIA alpha pixels over the opaque colour.
 	float4 oia = UnpackOia(alpha_depth);
-	base.rgb = base.rgb * (1.0f - oia.a) + oia.rgb;
+	base.rgb = base.rgb * (1.0f - oia.a) + oia.rgb * oia.a;
 
+	// Merge each alpha layer from back to front.
 	if (DepthOf(alpha_depth.w) != KBufferDepthFar)
 	{
 		float4 src = UnpackRGBA8(alpha_colour.w);
@@ -71,21 +73,21 @@ float4 PSAlphaResolve(PSIn In) :SV_Target
 }
 
 #ifdef PR_RDR_VSHADER_kbuffer_resolve
-PSIn main(uint vid :SV_VertexID)
+PSIn_KBufferResolve main(uint vid :SV_VertexID)
 {
 	return VSFullScreenTriangle(vid);
 }
 #endif
 
 #ifdef PR_RDR_PSHADER_kbuffer_depth_resolve
-float main(PSIn In) :SV_Depth
+float main(PSIn_KBufferResolve In) :SV_Depth
 {
 	return PSDepthResolve(In);
 }
 #endif
 
 #ifdef PR_RDR_PSHADER_kbuffer_alpha_resolve
-float4 main(PSIn In) :SV_Target
+float4 main(PSIn_KBufferResolve In) :SV_Target
 {
 	return PSAlphaResolve(In);
 }
